e9d66085449c90368f5974f3d03e9a7dc883f513
rubyleehs <rubyleehs@live.com>
2019-05-05 19:22:33 +0800

    LightningGE + WebGE

---
+++ Assets/Scripts/GridElements/LightningGE.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GridElement/LightningGE")]
+public class LightningGE : GridElement
+{
+    public override void Run(HexTile tile)
+    {
+        HexTile runTile = tile;
+        bool nextIsRight = false;
+        bool hasHitSomething = false;
+
+        while (!hasHitSomething)
+        {
+
+            if (nextIsRight) runTile = runTile.adjTiles[(int)Hexinal.SE];
+            else runTile = runTile.adjTiles[(int)Hexinal.SW];
+
+            if (runTile == null) hasHitSomething = true;
+            else
+            {
+                hasHitSomething = (runTile.ReadElementID() != -1);
+                runTile.UpdateElement(id);
+            }
+
+            nextIsRight = !nextIsRight;
+
+            TileVisualUpdateSequencer.AddToQueue(runTile);
+        }
+    }
+}
---
+++ Assets/Scripts/GridElements/WebGE.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GridElement/WebGE")]
+public class WebGE : GridElement
+{
+    public override void Run(HexTile tile)
+    {
+        HexTile[] runTiles = tile.adjTiles;
+        bool isDone = false;
+        while (!isDone)
+        {
+            isDone = true;
+            List<HexTile> visualUpdateTiles = new List<HexTile>();
+            for (int i = 0; i < 6; i++)
+            {
+                if (runTiles[i] == null) continue;
+                else
+                {
+                    visualUpdateTiles.Add(runTiles[i]);
+                    if (runTiles[i].ReadElementID() == -1)
+                    {
+                        runTiles[i].UpdateElement(id);
+                        isDone = false;
+                        runTiles[i] = runTiles[i].adjTiles[i];
+                    }
+                    else runTiles[i] = null;
+                }
+            }
+            TileVisualUpdateSequencer.AddToQueue(visualUpdateTiles);
+        }
+    }
+}
