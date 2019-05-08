bd6247db024479ac9964b3fa2a4d82ca2eeedf46
rubyleehs <rubyleehs@live.com>
2019-05-08 21:23:52 +0800

    Update HistoryManager to work with the changed OSs

--- Assets/Scripts/HistoryManager.cs
+++ Assets/Scripts/HistoryManager.cs
@@ -11,10 +11,9 @@ public struct GridChange//have to be a struct to store by value;
 
 public class HistoryManager : MonoBehaviour //Managers history of the board & element used
 {
+    private static List<InventoryTile> inventoryHistory;
     private static List<List<GridChange>> gridHistory;
     private static List<GridChange> gridPresent;
-
-    private static List<int> elementUsed;
 
     private static int currentPointOfTime = 0;
 
@@ -23,7 +22,7 @@ public class HistoryManager : MonoBehaviour //Managers history of the board & el
         gridHistory = new List<List<GridChange>>();
         gridPresent = new List<GridChange>();
 
-        elementUsed = new List<int>();
+        inventoryHistory = new List<InventoryTile>();
     }
 
     public static void AddToPresent(Vector2Int gridPos, int oriElementID, int changedElementID)
@@ -36,14 +35,14 @@ public class HistoryManager : MonoBehaviour //Managers history of the board & el
         });
     }
 
-    public static void AddPresentToHistory(int elementUsedID)
+    public static void AddPresentToHistory(InventoryTile inventoryElementUsed)
     {
         for (int i = currentPointOfTime + 1; i < gridHistory.Count;)
         {
             gridHistory.RemoveAt(i);
-            elementUsed.RemoveAt(i);
+            inventoryHistory.RemoveAt(i);
         }
-        elementUsed.Add(elementUsedID);
+        inventoryHistory.Add(inventoryElementUsed);
         gridHistory.Add(new List<GridChange>(gridPresent));
         gridPresent.Clear();
         currentPointOfTime = gridHistory.Count - 1;
@@ -55,7 +54,7 @@ public class HistoryManager : MonoBehaviour //Managers history of the board & el
         Debug.Log("Undo");
         BoardOS.bvus.ForceComplete();
 
-        InventoryOS.inventoryGrid.ElementIdToGrid(elementUsed[currentPointOfTime]).UpdateAvailability(true);
+        inventoryHistory[currentPointOfTime].UpdateAvailability(true);
         for (int i = 0; i < gridHistory[currentPointOfTime].Count; i++)
         {
             GridChange change = gridHistory[currentPointOfTime][i];
@@ -71,7 +70,7 @@ public class HistoryManager : MonoBehaviour //Managers history of the board & el
         Debug.Log("Redo");
         BoardOS.bvus.ForceComplete();
 
-        InventoryOS.inventoryGrid.ElementIdToGrid(elementUsed[currentPointOfTime + 1]).UpdateAvailability(false);
+        inventoryHistory[currentPointOfTime + 1].UpdateAvailability(false);
         for (int i = 0; i < gridHistory[currentPointOfTime + 1].Count; i++)
         {
             GridChange change = gridHistory[currentPointOfTime + 1][i];
