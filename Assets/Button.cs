9403f2bb2558686224cea5fa2a964952331c2730
rubyleehs <rubyleehs@live.com>
2019-05-25 13:20:27 +0800

    assets

---
+++ Assets/Button.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    private Image i;
+
+    private void Awake() {
+        i = GetComponent<Image>();
+        i.alphaHitTestMinimumThreshold = 0.01f;
+    }
+}
